9c12d7e00afa833006485b7e16c30ab6337c0265
Shane Hanna <shane.hanna@gmail.com>
2009-03-20 23:37:27 +1100

    Changed filter constructor API in anticipation of pipeline.

--- test/fragment_builder_test.rb
+++ test/fragment_builder_test.rb
@@ -4,21 +4,21 @@ class FragmentBuilderTest < Test::Unit::TestCase
   context 'XML::SAX::FragmentBuilder' do
 
     should 'call callback for record' do
-      builder = XML::SAX::FragmentBuilder.new(nil, {
+      builder = XML::SAX::FragmentBuilder.new(
         '//foo' => lambda do |el|
           assert_equal 'foo', el.name, 'foo element'
           assert_equal 1, el.parent.children.length, 'no siblings'
         end
-      })
+      )
       parser = Nokogiri::XML::SAX::PushParser.new(builder)
       parser << '<r><foo/><foo/><foo/></r>'
       parser.finish
     end
 
     should 'have buffered children for record' do
-      builder = XML::SAX::FragmentBuilder.new(nil, {
+      builder = XML::SAX::FragmentBuilder.new(
         '//foo' => lambda{|el| assert_equal 2, el.children.length}
-      })
+      )
       parser = Nokogiri::XML::SAX::PushParser.new(builder)
       parser << '<r><foo>text<el>el</el></foo></r>'
       parser.finish
